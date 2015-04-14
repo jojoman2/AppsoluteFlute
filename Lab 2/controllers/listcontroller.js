0711cce7df51920d6d27daa08a4b4f9be6026b04
Fredrik <fredde_wallen@hotmail.com>
2015-04-14 12:49:23 +0200

    Continued

--- Lab 2/controllers/listcontroller.js
+++ Lab 2/controllers/listcontroller.js
@@ -2,12 +2,22 @@ newsFeedApp.controller('ListCtrl', function($rootScope, $scope){
     $scope.newsItems = [];
 
     var feed = new google.feeds.Feed('http://www.rendip.com/rss');
-    feed.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
+    feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
     feed.load(function(result){
-        console.log(result);
         if (!result.error) {
             $scope.newsItems = result.feed.entries;
+            for (var i = 0; i < result.feed.entries.length; i++) {
+                entry = result.feed.entries[i];
+                var mediaEntries = entry.xmlNode.getElementsByTagNameNS('*', 'thumbnail');
+                for (var j = 0; j < mediaEntries.length; j++) {
+                    var mediaEntry = mediaEntries[j];
+                    var mediaThumbnailUrl = mediaEntry.attributes.getNamedItem('url').value
+                    $scope.newsItems[i]['image'] = mediaThumbnailUrl;
+                }
+            }
+            console.log($scope.newsItems);
             $scope.$apply();
+
 
         }
 
