b114b9ca519f50d07fdc707af38235291d81022f
mariliab <marilia.bognandi@gmail.com>
2015-04-22 11:23:51 +0200

    detail View

--- Lab 2/controllers/listcontroller.js
+++ Lab 2/controllers/listcontroller.js
@@ -2,7 +2,7 @@ newsFeedApp.controller('ListCtrl', function($rootScope, $scope){
     $scope.newsItems = [];
 
     var feed = new google.feeds.Feed('http://www.rendip.com/rss');
-    feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
+    feed.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
     feed.load(function(result){
         if (!result.error) {
             $scope.newsItems = result.feed.entries;
