5bfcba2fb128b59afb6431264e7bad03054325d9
mariliab <marilia.bognandi@gmail.com>
2015-04-24 17:19:26 +0200

    labb 3

---
+++ Lab_3/pubNub.js
@@ -0,0 +1,14 @@
+PUBNUB_demo.subscribe({
+    channel: 'demo_tutorial',
+    message: function(m){console.log(m)}
+});
+
+$("inputText").submit(function(){
+	var inputText = $("inputTextField").text();
+	
+	PUBNUB_demo.publish({
+    channel: 'demo_tutorial',
+    message: {"color":"blue"}
+});
+
+});
