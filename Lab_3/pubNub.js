0d46c9462c38eac5e2ef673ecb751f79fd1f45a4
Fredrik <fredde_wallen@hotmail.com>
2015-04-24 18:27:03 +0200

    Tried to make the compass work

--- Lab_3/pubNub.js
+++ Lab_3/pubNub.js
@@ -1,4 +1,25 @@
 $( document ).ready(function() {
+    Compass.noSupport(function () {
+        //$('.compass').hide();
+        $('#coordinateInstruction').text("No support for compass");
+    });
+
+    Compass.needGPS(function () {
+        //$('.go-outside-message').show();          // Step 1: we need GPS signal
+        $('#coordinateInstruction').text("Go outside");
+    }).needMove(function () {
+        //$('.go-outside-message').hide()
+        //$('.move-and-hold-ahead-message').show(); // Step 2: user must go forward
+        $('#coordinateInstruction').text("Move and hold ahead");
+    }).init(function () {
+        //$('.move-and-hold-ahead-message').hide(); // GPS hack is enabled
+        $('#coordinateInstruction').text("GPS HACK");
+    });
+
+    Compass.watch(function (heading) {
+        $('#coordinates2').text(heading);
+        //$('.compass').css('transform', 'rotate(' + (-heading) + 'deg)');
+    });
     PUBNUB_demo.subscribe({
         channel: 'chatMessages',
         message: function (m) {
@@ -15,6 +36,7 @@ $( document ).ready(function() {
             channel: 'chatMessages',
             message: {"text":inputText}
         });
+
 });
 
 
